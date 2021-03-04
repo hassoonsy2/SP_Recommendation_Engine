5f2cf7a8a85fcfbf51f5d14b924ae76416ed1ac8
hassoonsy2 <71430169+hassoonsy2@users.noreply.github.com>
2021-03-04 16:33:03 +0100

    04.03

--- Mongo-Postgres script.sql
+++ Mongo-Postgres script.sql
@@ -1,11 +1,10 @@
 DROP TABLE IF EXISTS product CASCADE;
 DROP TABLE IF EXISTS prijs CASCADE ;
 DROP TABLE IF EXISTS properties CASCADE ;
-
-DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS profiles CASCADE;
+DROP TABLE IF EXISTS profiels CASCADE;
+DROP TABLE IF EXISTS  orders CASCADE;
 DROP TABLE IF EXISTS sessions CASCADE;
-
+DROP TABLE IF EXISTS profiel_recommndations CASCADE;
 
 
 
@@ -38,3 +37,55 @@ variant varchar(300),
 FOREIGN KEY(id_properties) REFERENCES product(id_product) on delete cascade on update cascade ,
 PRIMARY KEY(id_properties)
 );
+
+
+CREATE TABLE profiels(
+	id_profiel varchar(300) NOT NULL,
+	browsersid TEXT [] unique ,
+	segment varchar(300),
+	first_date timestamp  , 
+	last_date timestamp ,
+	PRIMARY KEY(id_profiel)
+
+);
+
+CREATE TABLE profiel_recommndations(
+	id_profiel varchar(300) NOT NULL,
+	viewed_before_id varchar array,
+	similars_id varchar array,
+	previously_recommended varchar array,
+	FOREIGN KEY(id_profiel) REFERENCES profiels(id_profiel) on delete cascade on update cascade
+	
+);	
+
+CREATE TABLE sessions(
+	session_id varchar(300)NOT NULL,
+	profile_id varchar (300), 
+	session_start timestamp,
+	session_end timestamp,
+	has_sale boolean ,
+	user_agent_browser varchar(300),
+	user_agent_device varchar(300),
+	segment_sessions varchar(300),
+	utm_source varchar(300),
+	event_source varchar(300),
+	event_action varchar(300),
+	PRIMARY KEY(session_id),
+	FOREIGN KEY(profile_id) REFERENCES profiels(id_profiel) on delete cascade on update cascade 
+
+	
+
+);	
+
+CREATE TABLE orders(
+	id_product varchar(300)NOT NULL,
+	id_profiel_o varchar(300) NOT NULL,
+	PRIMARY KEY(id_product),
+	FOREIGN KEY(id_profiel_o) REFERENCES profiels(id_profiel) on delete cascade on update cascade 
+);
+
+
+
+
+	
+	
